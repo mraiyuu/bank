ceb626f912e5f49ea68f97afaf4400b4a2283568
mraiyuu <munenedenis618@gmail.com>
2024-01-26 14:30:33 +0300

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,15 +39,10 @@ const Footer = () => (
         ))}
       </div>
     </div>
-    <div>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-#[3F3E45]">
       <p className="font-poppins font-normal text-center text-white text-[18px] leading-[27px]">
         2021 HooBank. All Rights Reserved.
       </p>
-      {/* <div className="flex flex-row md:mt-0 mt-6">
-        {socialMedia.map((social, key) => (
-          <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] cursor-pointer object-contain ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`} />
-        ))}
-      </div> */}
 
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
@@ -58,6 +53,7 @@ const Footer = () => (
             className={`w-[21px] h-[21px] object-contain cursor-pointer ${
               index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
             }`}
+            onClick={() => window.open(social.link)}
           />
         ))}
       </div>
